b86f10eaf68e9fc9c4b6f10bba900203b2ae60ee
Rigie <rigiechang@gmail.com>
2019-06-26 19:54:38 -0700

    Fetch from REST Api and Display
    http://localhost:6060/

--- App.jsx
+++ App.jsx
@@ -1,10 +1,53 @@
 import React from 'react'; 
  
 class App extends React.Component { 
+
+      componentDidMount() {
+        fetch('http://localhost:4000/admin/products')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ products: data });
+		  console.log("products data:");
+		  console.log(data);
+        })
+        .catch(console.log)
+      }
+
+
+	constructor(props){
+		super();
+		this.state = {
+			hero: "Superman",
+			products: []
+		};
+		
+	}
+
    render() { 
       return ( 
          <div> 
+		 
+		 {/* A JSX comment 
+
             Hello World!!! 
+			<br /><br />
+			
+			{this.state.hero}
+			<br /><br />
+		*/}	
+			
+          {this.state.products.map((product) => (
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">{product.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{product.category}</h6>
+                <p class="card-text">${product.price}</p>
+				<p class="card-text">{product.vendor}</p>
+				<p class="card-text">{product.imageUrl}</p>
+              </div>
+            </div>
+          ))}	
+			
          </div> 
       ); 
    } 
